41fc6f7576e67b1382468d7c58c574b54a09432b
Shravan Kumar <40226836+shravan341@users.noreply.github.com>
2025-04-24 15:44:52 +0530

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env sh
 
-kill $(cat .pidfile)
+if [ -f .pidfile ]; then
+  PID=$(cat .pidfile)
+
+  if ps -p $PID > /dev/null 2>&1; then
+    echo "Killing process $PID"
+    kill -9 $PID
+  else
+    echo "Process $PID not running."
+  fi
+
+  rm .pidfile
+else
+  echo ".pidfile not found. Nothing to kill."
+fi
