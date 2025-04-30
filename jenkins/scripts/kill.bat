206fa461f269bc5a34c2903abbe5bfb1d0ed4341
Shravan Kumar <40226836+shravan341@users.noreply.github.com>
2025-04-30 22:46:15 +0530

    Update kill.bat

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,16 +1,3 @@
-#!/usr/bin/env sh
-
-if [ -f .pidfile ]; then
-  PID=$(cat .pidfile)
-
-  if ps -p $PID > /dev/null 2>&1; then
-    echo "Killing process $PID"
-    kill -9 $PID
-  else
-    echo "Process $PID not running."
-  fi
-
-  rm .pidfile
-else
-  echo ".pidfile not found. Nothing to kill."
-fi
+@echo off
+taskkill /F /IM node.exe /T > nul 2>&1
+exit 0
